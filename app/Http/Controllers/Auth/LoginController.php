7cc4a9797ed9e5e9b9ec7b480f95ccc97b76a47d
jora <jora.shahbazyan@betconstruct.com>
2021-09-21 13:13:40 +0400

    fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use App\Constanats\UserRoleConstants;
+//use App\Constanats\UserRoleConstants;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -39,7 +39,7 @@ class LoginController extends Controller
 
     protected function authenticated(Request $request, $user)
     {
-        if ($user->role === UserRoleConstants::SuperAdmin) {
+        if ($user->role === \App\Constanats\UserRoleConstants::SuperAdmin) {
             return redirect('/companies');
         }
 
